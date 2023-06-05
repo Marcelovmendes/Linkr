c8821a62f438abb3ce815485d5b5d55ce4a34b9f
Igor Guimarães <igorgui.tec@gmail.com>
2023-06-05 16:53:55 -0300

    Empty Auth Element Warning

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -6,6 +6,7 @@ import { login } from "../../services/api";
 
 export default function SignIn() {
     const [form, setForm] = useState({})
+    const [disabled, setDisabled] = useState(false)
     const navigate = useNavigate();
     function handleForm(e) {
         setForm({ ...form, [e.target.name]: e.target.value })
@@ -13,6 +14,11 @@ export default function SignIn() {
     }
     function handleSubmit(e) {
         e.preventDefault()
+        setDisabled(!disabled);
+        let empty = '';
+        if(!form.email) empty += 'email, '
+        if(!form.password) empty += 'password';
+        if(empty) return alert(`Preencha os campos: ${empty}`)
         login(form)
             .then(res => {
                 alert(res.data)
@@ -29,7 +35,7 @@ export default function SignIn() {
             <FormStyle onSubmit={handleSubmit}>
                 <Input placeholder="e-mail" name="email" onChange={(e) => handleForm(e)} />
                 <Input placeholder="password" type="password" name="password" onChange={(e) => handleForm(e)} />
-                <Button type="submit" value="Log In" />
+                <Button type="submit" value="Log In" disabled={disabled} />
                 <Link to="/sign-up">First time? Create an account!</Link>
             </FormStyle>
         </MainComponentStyle>
--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -6,6 +6,7 @@ import { logUp } from "../../services/api";
 
 export default function SignUp() {
     const [form, setForm] = useState({})
+    const [disabled, setDisabled] = useState(false)
     const navigate = useNavigate()
 
 
@@ -14,7 +15,14 @@ export default function SignUp() {
         console.log(form)
     }
     function handleSubmit(e) {
-        e.preventDefault()
+        e.preventDefault();
+        let empty = '';
+        if(!form.email) empty += 'email, ';
+        if(!form.password) empty += 'password, ';
+        if(!form.name) empty += 'name, ';
+        if(!form.picture_url) empty += 'picture_url';
+        if(empty) return alert(`Preencha os campos: ${empty}`);
+        setDisabled(!disabled);
         logUp(form)
             .then(res => {
                 alert(res.data)
@@ -33,7 +41,7 @@ export default function SignUp() {
                 <Input placeholder="password" type="password" name="password" onChange={(e) => handleForm(e)} />
                 <Input placeholder="username" name="name" onChange={(e) => handleForm(e)} />
                 <Input placeholder="picture url" name="picture_url" onChange={(e) => handleForm(e)} />
-                <Button type="submit" value="Sign Up" />
+                <Button type="submit" value="Sign Up" disabled={disabled} />
                 <Link to="/">Switch back to log in</Link>
             </FormStyle>
         </MainComponentStyle>
